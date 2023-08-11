03342652784f80b8e95c4c4daa2fc2771e608f8e
BillyMendoza <billymigz22@gmail.com>
2023-08-11 12:56:02 +0800

    ConnectionString & Adding the DbContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace C_WebApiTraining.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 global using C_WebApiTraining.Models;
+using C_WebApiTraining.Data;
 using C_WebApiTraining.Services.CharacterService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
